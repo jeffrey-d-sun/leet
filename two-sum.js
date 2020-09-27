9dc93ec157c7597396ce945bf9f9ea9d2202fcfc
jeffrey d sun <jeffreydsun90@gmail.com>
2020-09-27 12:38:38 -0700

    add two-sum solution

--- two-sum.js
+++ two-sum.js
@@ -1,22 +1,4 @@
 const twoSum = (nums, target) => {
-  // two-pass hash table solution
-  /*
-  const hashMap = nums.reduce((acc, num, index) => {
-    acc[num] = index
-    return acc
-  }, {})
-
-  const res = []
-  for (let i = 0; i < nums.length; i++) {
-    const num = nums[i]
-    if (hashMap[target - num] && hashMap[target - num] !== i) {
-      res.push(i, hashMap[target - num])
-      break;
-    }
-  }
-  return res
-  */
-
   // one-pass hash table solution
   const hash = {}
   const result = []
