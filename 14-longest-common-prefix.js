75f3fbb0b24bffacbcc5a35cc9b06dbaed938e91
jeffrey d sun <jeffreydsun90@gmail.com>
2020-11-08 20:00:03 -0800

    Add solution to longest common prefix

---
+++ 14-longest-common-prefix.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  if (!strs.length) return ''
+  strs.sort((a, b) => a.length - b.length)
+  const shortestWord = strs.shift()
+  let longestPrefix = ''
+  
+  for (let i = 0; i < shortestWord.length; i++) {
+    const letter = shortestWord[i]
+    
+    for (let j = 0; j < strs.length; j++) {
+      const str = strs[j]
+      if (shortestWord.substring(0, i+1) !== str.substring(0, i+1)) {
+        return longestPrefix
+      }
+    }
+    
+    longestPrefix += letter
+  }
+  return longestPrefix
+};
+
+console.log(longestCommonPrefix(['flower', 'flow', 'flight'])) // 'fl'
+console.log(longestCommonPrefix(['dog', 'racecar', 'car'])) // ''
