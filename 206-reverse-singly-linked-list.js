c56b5a66379e00fa5ea4282101829fc3c891a156
jeffrey d sun <jeffreydsun90@gmail.com>
2020-11-21 20:02:47 -0800

    Add solution for 206 reverse singly linked list

---
+++ 206-reverse-singly-linked-list.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const isAnagram = (s, t) => {
+  if (s.length !== t.length) return false
+  const hash = {}
+  for(let c in s) {
+    hash[s[c]] = (hash[s[c]] || 0) + 1
+  }
+  
+  for(let c in t) {
+    if (hash[t[c]] === undefined) {
+      return false
+    }
+    if (hash[t[c]] > 0) {
+      hash[t[c]] -= 1
+    }
+  }
+  
+  return Object.values(hash).every(count => count === 0)
+};
