ce49a6968273235bffb8782ac8486f4b555bd8bc
jeffrey d sun <jeffreydsun90@gmail.com>
2020-11-21 17:50:58 -0800

    Add solution for 70 climbing stairs

---
+++ 70-climbing-stairs.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+// O(n) time
+// O(1) space
+
+const climbStairs = (n) => {
+  if (n === 1 || n === 0) return 1
+  let memo = [1, 1]
+  
+  for (let i = 2; i <= n; i++) {
+    [memo[0], memo[1]] = [memo[1], memo[0] + memo[1]]
+  }
+  
+  return memo[1]
+};
+
+console.log(climbStairs(2)) // 2
+console.log(climbStairs(3)) // 3
+console.log(climbStairs(4)) // 5
+console.log(climbStairs(5)) // 8
+console.log(climbStairs(6)) // 13
