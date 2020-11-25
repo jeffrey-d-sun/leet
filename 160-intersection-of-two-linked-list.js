20c603133504bd0edd8ea397150397bec3c1d5e7
jeffrey d sun <jeffreydsun90@gmail.com>
2020-11-24 23:04:04 -0800

    Refactor solution

--- 160-intersection-of-two-linked-list.js
+++ 160-intersection-of-two-linked-list.js
@@ -15,22 +15,9 @@ const getIntersectionNode = (headA, headB) => {
   let p1 = headA
   let p2 = headB
   
-  while(p1 !== null && p2 !== null) {
-    if (p1 === p2 && p1 !== 0) {
-      return p1
-    }
-    
-    if (p1 && p1.next === null) {
-      p1 = headB
-      p2 = p2.next
-    } else if (p2 && p2.next === null) {
-      p2 = headA
-      p1 = p1.next
-    } else {
-      p1 = p1.next
-      p2 = p2.next
-    }
+  while (p1 !== p2)  {
+    p1 = p1 === null ? headB : p1.next
+    p2 = p2 === null ? headA : p2.next
   }
-  
-  return null
+  return p1
 };
